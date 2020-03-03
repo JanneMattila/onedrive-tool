a59aa06aa4c68e0873989584a81a6ec6acc47a1a
Janne Mattila <janne.mattila@hotmail.fi>
2020-03-03 18:41:55 +0200

    Add recursive file scanning logic

--- src/OneDriveCleaner/OneDriveManager.cs
+++ src/OneDriveCleaner/OneDriveManager.cs
@@ -10,6 +10,8 @@ namespace OneDriveCleaner
 	public class OneDriveManager
     {
 		private string _accessToken;
+		private GraphServiceClient _graphserviceClient;
+		private List<DriveItem> _items = new List<DriveItem>();
 
 		public OneDriveManager()
 		{
@@ -23,11 +25,7 @@ namespace OneDriveCleaner
 				.Build();
 			var authenticationResult = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
 			_accessToken = authenticationResult.AccessToken;
-		}
-
-		public async Task ScanAsync()
-		{
-			var graphserviceClient = new GraphServiceClient(
+			_graphserviceClient = new GraphServiceClient(
 				new DelegateAuthenticationProvider(
 					(requestMessage) =>
 					{
@@ -35,14 +33,55 @@ namespace OneDriveCleaner
 							new AuthenticationHeaderValue("Bearer", _accessToken);
 						return Task.FromResult(0);
 					}));
+		}
 
-			var driveRequest = graphserviceClient
+		public async Task ScanAsync()
+		{
+			var driveRequest = _graphserviceClient
+				.Me
 				.Drive
 				.Request();
 			var driveResponse = await driveRequest.GetAsync();
 
 			var used = Math.Round(driveResponse.Quota.Used.Value / Math.Pow(2, 30), 0);
 			Console.WriteLine($"Drive contains {used} GB of data");
+
+			var rootRequest = _graphserviceClient
+				.Me
+				.Drive
+				.Root
+				.Children
+				.Request();
+			await ProcessFiles(rootRequest);
+		}
+
+		private async Task ProcessFiles(IDriveItemChildrenCollectionRequest childrenCollectionRequest)
+		{
+			var driveItems = await childrenCollectionRequest.GetAsync();
+			var items = new List<DriveItem>(driveItems.CurrentPage);
+			while(driveItems.NextPageRequest != null)
+			{
+				var request = driveItems
+					.NextPageRequest;
+				var response = await request.GetAsync();
+
+				items.AddRange(response.CurrentPage);
+			}
+
+			foreach (var item in items)
+			{
+				if (item.Folder != null)
+				{
+					var request = _graphserviceClient
+						.Me
+						.Drive
+						.Items[item.Id]
+						.Children
+						.Request();
+					await ProcessFiles(request);
+				}
+			}
+			_items.AddRange(items);
 		}
 	}
 }
