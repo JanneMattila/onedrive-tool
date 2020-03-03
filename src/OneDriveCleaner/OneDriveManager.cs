9bc50e618b0a5fe6070a10da8efc101a338c7c88
Janne Mattila <janne.mattila@hotmail.fi>
2020-03-03 20:45:51 +0200

    Add simple analyzes for the duplicates

--- src/OneDriveCleaner/OneDriveManager.cs
+++ src/OneDriveCleaner/OneDriveManager.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using CsvHelper;
 using Microsoft.Graph;
@@ -46,6 +47,27 @@ namespace OneDriveCleaner
 			_items = csv.GetRecords<File>().ToList();
 
 			Console.WriteLine($"{_items.Count} files to analyze.");
+			var hashes = new Dictionary<string, List<File>>();
+			foreach (var item in _items)
+			{
+				if (!hashes.ContainsKey(item.Sha1Hash))
+				{
+					hashes.Add(item.Sha1Hash, new List<File>());
+				}
+				hashes[item.Sha1Hash].Add(item);
+			}
+
+			var duplicates = hashes
+				.Where(h => h.Value.Count > 1)
+				.Sum(h => h.Value.Count - 1);
+			Console.WriteLine($"{duplicates} total duplicate files.");
+
+			const int top = 25;
+			var topDuplicates = string.Join(", ", hashes
+				.OrderByDescending(h => h.Value.Count)
+				.Select(h => h.Value.Count)
+				.Take(top));
+			Console.WriteLine($"Top {top} duplicate counts: {topDuplicates}.");
 		}
 
 		public async Task ScanAsync()
